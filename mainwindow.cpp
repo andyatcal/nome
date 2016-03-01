3885df876f411c50908c41ee5d9b4c184c1834ab
andyatcal <andyatcal@berkeley.edu>
2016-02-29 18:54:14 -0800

    arcball done

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,15 +3,30 @@
 MainWindow::MainWindow(QWidget *parent) :
     QGLWidget(parent)
 {
-    setMouseTracking(true);
+    startTimer(0);
     makeDefaultMesh();
     cameraDistance = 4;
+    last_mx = last_my = cur_mx = cur_my = 0;
+    arcball_on = false;
 }
 
 void MainWindow::makeDefaultMesh()
 {
-    makeCube(mesh,0.5,0.5,0.5);
-    mesh.computeNormals();
+    makeCube(master_mesh,0.5,0.5,0.5);
+    master_mesh.computeNormals();
+}
+
+vec3 MainWindow::get_arcball_vector(int x, int y) {
+    vec3 p = vec3(1.0 * x / this->width() * 2 - 1.0,
+      1.0 * y / this->height() * 2 - 1.0, 0);
+    p.y = - p.y;
+    float op_squared = p.x * p.x + p.y * p.y;
+    if (op_squared <= 1 * 1) {
+        p.z = sqrt(1 * 1 - op_squared);
+    } else {
+        p = normalize(p);  // nearest point
+    }
+    return p;
 }
 
 MainWindow::~MainWindow()
@@ -56,30 +71,78 @@ void MainWindow::paintGL()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     gluLookAt(0, 0, cameraDistance, 0, 0, 0, 0, 1, 0);
-    glMultMatrixf(&mesh.object2world[0][0]);
+    glMultMatrixf(&master_mesh.object2world[0][0]);
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, RED);
-    mesh.drawMesh();
+    master_mesh.drawMesh();
     glutSwapBuffers();
 }
 
 void MainWindow::mousePressEvent(QMouseEvent* event)
 {
-
+    if (event->buttons() & Qt::LeftButton)
+    {
+        arcball_on = true;
+        last_mx = cur_mx = event -> x();
+        last_my = cur_my = event -> y();
+    }
+    else
+    {
+        arcball_on = false;
+    }
 }
 
 void MainWindow::mouseMoveEvent(QMouseEvent* event)
 {
-    printf("%d, %d\n", event->x(), event->y());
+    if(arcball_on)
+    {
+      cur_mx = event -> x();
+      cur_my = event -> y();
+    }
 }
 
+void MainWindow::timerEvent(QTimerEvent *event) {
+    if(last_mx != cur_mx || last_my != cur_my) {
+        vec3 va = get_arcball_vector(last_mx, last_my);
+        vec3 vb = get_arcball_vector( cur_mx,  cur_my);
+        float angle = acos(glm::min(1.0f, dot(va, vb)));
+        vec3 axis_in_camera_coord = cross(va, vb);
+        mat3 camera2object = inverse(mat3(transforms[MODE_CAMERA]) * mat3(master_mesh.object2world));
+        vec3 axis_in_object_coord = camera2object * axis_in_camera_coord;
+        master_mesh.object2world = rotate(master_mesh.object2world, angle, axis_in_object_coord);
+        last_mx = cur_mx;
+        last_my = cur_my;
+    }
+    //fix_subdivision_matrices();
+    repaint();
+}
 void MainWindow::keyPressEvent(QKeyEvent* event)
 {
     switch(event->key()) {
     case Qt::Key_Escape:
         close();
         break;
+    case Qt::Key_W:
+        if (wireframe) {
+            glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+        } else {
+            glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+        }
+        wireframe = !wireframe;
+        cout<<"Hey!"<<endl;
+        break;
+    case Qt::Key_I:
+        if(cameraDistance > 0.1) {
+            cameraDistance *= 0.9;
+        }
+        break;
+    case Qt::Key_O:
+        if(cameraDistance < 200) {
+            cameraDistance *= 1.1;
+        }
+        break;
     default:
         event->ignore();
         break;
     }
+    glutPostRedisplay();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -30,12 +30,26 @@ public:
 
 private:
     // Viewer variables.
-    Mesh mesh;
     enum MODES { MODE_OBJECT, MODE_CAMERA, MODE_LIGHT, MODE_LAST } view_mode;
     mat4 transforms[MODE_LAST];
     float cameraDistance;
+    int last_mx, last_my, cur_mx, cur_my;
+    int arcball_on;
+    bool wireframe;
     void makeDefaultMesh();
-
+    Mesh master_mesh;
+    Mesh temp_mesh;
+    /**
+     * Get a normalized vector from the center of the virtual ball O to a
+     * point P on the virtual ball surface, such that P is aligned on
+     * screen's (X,Y) coordinates.  If (X,Y) is too far away from the
+     * sphere, return the nearest point on the virtual ball surface.
+     */
+    vec3 get_arcball_vector(int x, int y);
+    /**
+     * Timer Event for this MainWindow. Similar to OnIdleFunc of GLUT.
+     */
+    void timerEvent(QTimerEvent *event);
 protected:
     void initializeGL();
     void resizeGL(int w, int h);
